15d36acd52e159fd1b945304ac183adba7b66eae
Gonen Matias <36644833+source144@users.noreply.github.com>
2020-03-28 00:11:04 -0400

    Delete run.h
    not needed

--- run.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PL0RUN__
-
-#include <stdio.h> 
-#include "pvm.h"
-
-instruction_t **compile(char *sourcecodePath, int lexFlag, int asmblyFlag);
-void run(instruction_t **code, int runFlag);
-
-#endif
